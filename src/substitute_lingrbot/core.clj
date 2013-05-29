d8836eec948c21ef373c72e75d866d3beb0bb351
ujihisa <ujihisa@gmail.com>
2013-05-29 00:39:11 -0700

    format

--- src/substitute_lingrbot/core.clj
+++ src/substitute_lingrbot/core.clj
@@ -23,22 +23,18 @@
         :let [text (:text message)
               nick (:nickname message)]]
     (if (re-find #"^!help$" text)
-      "s/regexp/text/
+      "* s/regexp/text/
        replaces the latest previous message.
-
        s/regexp/text/ < nickname
        replaces the latest previous message by the nickname user.
-
-       s/regexp/text/ < @id
+       * s/regexp/text/ < @id
        replaces the latest previous message by the id user.
        NOT IMPLMENENTED YET
-
-       s/regexp/text/ < me
+       * s/regexp/text/ < me
        replaces the latest previous message by yourself.
        NOT IMPLMENENTED YET
-
-       (NOT IMPLEMENTED) it looks up older messages if regexp didn't match
-       http://substitute-lingrbot.herokuapp.com/"
+       * (NOT IMPLEMENTED) it looks up older messages if regexp didn't match
+       * http://substitute-lingrbot.herokuapp.com/"
       (if-let [[_ left right _ target-nick]
                (re-find #"^s/((?:\\.|[^/])+)/((?:\\.|[^/])*)/g?\s*(<\s*@?(.*))?$" text)]
         (if target-nick
