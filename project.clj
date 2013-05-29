932b5a93bd633bffc602f1836af8fdfa6a8c321f
ujihisa <ujihisa@gmail.com>
2013-05-28 22:14:44 -0700

    initial implementation

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject substitute-lingrbot "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "GPL-3+"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/data.json "0.1.2"]
+                 [compojure "1.0.0-RC2"]
+                 [ring "1.0.1"]]
+  :main clojure-lingrbot.core)
---
+++ src/substitute_lingrbot/core.clj
@@ -0,0 +1,38 @@
+(ns substitute-lingrbot.core
+  (:require [leiningen.core.project])
+  (:use [compojure.core :only (defroutes GET POST)]
+        [clojure.data.json :only (read-json)]
+        [ring.adapter.jetty :only (run-jetty)])
+  (:import java.util.concurrent.ExecutionException)
+  (:gen-class))
+
+(def version
+  (:version (leiningen.core.project/read)))
+
+(def start-time
+  (java.util.Date.))
+
+(def previous-message (atom {}))
+
+(defroutes routes
+  (GET "/" []
+       (str {:version version
+             :homepage "https://github.com/ujihisa/substitute-lingrbot"
+             :from start-time
+             :author "ujihisa"}))
+  (POST "/" {body :body headers :headers}
+        (when (ACCEPTED_IPS (headers "x-forwarded-for"))
+          (let [results
+                (for [message (map :message (:events (read-json (slurp body))))
+                      :let [text (:text message)
+                            room (:room message)]]
+                  (if (re-find #"^s/([^/]+)/([^/]+)/g?$" message)
+                    (format "OK %s" (get @previous-message room))
+                    (swap! previous-message assoc room text)))]
+            (clojure.string/join "\n" results)))))
+
+(defn -main []
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "8080"))]
+    (run-jetty routes {:port port :join? false})))
+
+; vim: set lispwords+=defroutes :
