31a10c07aeca85c0221165856951e7b31ff05771
ujihisa <ujihisa@gmail.com>
2013-05-28 22:24:13 -0700

    heroku--

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject substitute-lingrbot "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "GPL-3+"}
+  :license {:name "GPL-3+"
+            :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.1.2"]
                  [compojure "1.0.0-RC2"]
                  [ring "1.0.1"]]
+  :min-lein-version "2.1.0"
   :main substitute-lingrbot.core)
