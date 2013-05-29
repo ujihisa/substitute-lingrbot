c98e2a144933c929f0bc8e3ea997c0bad108d67e
ujihisa <ujihisa@gmail.com>
2013-05-28 22:24:43 -0700

    dependency upgrade

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.1.2"]
-                 [compojure "1.0.0-RC2"]
-                 [ring "1.0.1"]]
+                 [compojure "1.1.3"]
+                 [ring/ring-jetty-adapter "1.1.6"]]
   :min-lein-version "2.1.0"
   :main substitute-lingrbot.core)
