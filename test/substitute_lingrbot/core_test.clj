41be5762298d3704867213728cf649da1c47fa12
ujihisa <ujihisa@gmail.com>
2015-07-31 23:08:29 -0700

    test skeleton

---
+++ test/substitute_lingrbot/core_test.clj
@@ -0,0 +1,7 @@
+(ns substitute-lingrbot.core-test
+  (:require [clojure.test :refer :all]
+            [substitute-lingrbot.core :refer :all]))
+
+(deftest aaaaaa-test
+  (testing "bbbbbb"
+    (is (= 1 1))))
