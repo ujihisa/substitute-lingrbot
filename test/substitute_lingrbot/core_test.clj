e3e5dca4e9aad8aa6bb8c2aafff8733aeecd4ece
ujihisa <ujihisa@gmail.com>
2015-08-05 20:05:48 -0700

    specify nickname

--- test/substitute_lingrbot/core_test.clj
+++ test/substitute_lingrbot/core_test.clj
@@ -2,21 +2,22 @@
   (:require [clojure.test :refer :all]
             [substitute-lingrbot.core :refer :all]))
 
-(defn- dummy-lingr-msg [text room]
+(defn- dummy-lingr-msg [text room nickname]
   (format "{\"events\":[{\"message\":{\"text\":%s,\"nickname\":\"aaa\",\"room\":\"%s\"}}]}"
           (prn-str text)
-          (str room)))
+          (str room)
+          (str nickname)))
 
 (deftest handle-post-test
   (testing "ignore non-substitute messages"
-    (is (= [""] (handle-post (dummy-lingr-msg "hello" :room1)) ))
-    (is (= [""] (handle-post (dummy-lingr-msg "world" :room1)))))
+    (is (= [""] (handle-post (dummy-lingr-msg "hello" :room1 :you))))
+    (is (= [""] (handle-post (dummy-lingr-msg "world" :room1 :you)))))
 
   (testing "substitute previous message"
-    (is (= [""] (handle-post (dummy-lingr-msg "hello" :room1)) ))
-    (is (= ["he__o"] (handle-post (dummy-lingr-msg "s/l/_/" :room1)))))
+    (is (= [""] (handle-post (dummy-lingr-msg "hello" :room1 :you)) ))
+    (is (= ["he__o"] (handle-post (dummy-lingr-msg "s/l/_/" :room1 :you)))))
 
   (testing "substitute previous message, ignoring message of another room"
-    (is (= [""] (handle-post (dummy-lingr-msg "hello" :room1)) ))
-    (is (= [""] (handle-post (dummy-lingr-msg "world" :room2)) ))
-    (is (= ["hell_"] (handle-post (dummy-lingr-msg "s/o/_/" :room1))))))
+    (is (= [""] (handle-post (dummy-lingr-msg "hello" :room1 :you)) ))
+    (is (= [""] (handle-post (dummy-lingr-msg "world" :room2 :you)) ))
+    (is (= ["hell_"] (handle-post (dummy-lingr-msg "s/o/_/" :room1 :you))))))
