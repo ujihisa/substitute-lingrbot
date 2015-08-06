e39b77165c27c6b97de46a0d60c6d123a507fb19
0xBADDCAFE <0xbdcf@gmail.com>
2015-08-06 13:45:26 +0900

    room and nickname are placed in reverse order

--- test/substitute_lingrbot/core_test.clj
+++ test/substitute_lingrbot/core_test.clj
@@ -10,14 +10,14 @@
 
 (deftest handle-post-test
   (testing "ignore non-substitute messages"
-    (is (= [""] (handle-post (dummy-lingr-msg "hello" :room1 :you))))
-    (is (= [""] (handle-post (dummy-lingr-msg "world" :room1 :you)))))
+    (is (= [""] (handle-post (dummy-lingr-msg "hello" :you :room1))))
+    (is (= [""] (handle-post (dummy-lingr-msg "world" :you :room1)))))
 
   (testing "substitute previous message"
-    (is (= [""] (handle-post (dummy-lingr-msg "hello" :room1 :you)) ))
-    (is (= ["he__o"] (handle-post (dummy-lingr-msg "s/l/_/" :room1 :you)))))
+    (is (= [""] (handle-post (dummy-lingr-msg "hello" :you :room1)) ))
+    (is (= ["he__o"] (handle-post (dummy-lingr-msg "s/l/_/" :you :room1)))))
 
   (testing "substitute previous message, ignoring message of another room"
-    (is (= [""] (handle-post (dummy-lingr-msg "hello" :room1 :you)) ))
-    (is (= [""] (handle-post (dummy-lingr-msg "world" :room2 :you)) ))
-    (is (= ["hell_"] (handle-post (dummy-lingr-msg "s/o/_/" :room1 :you))))))
+    (is (= [""] (handle-post (dummy-lingr-msg "hello" :you :room1)) ))
+    (is (= [""] (handle-post (dummy-lingr-msg "world" :you :room2)) ))
+    (is (= ["hell_"] (handle-post (dummy-lingr-msg "s/o/_/" :you :room1))))))
