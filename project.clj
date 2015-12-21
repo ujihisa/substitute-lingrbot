d9192b60233ce81f0bafff3cdf2391d7ad5c8ad2
ujihisa <ujihisa@gmail.com>
2015-12-20 23:21:05 -0800

    Update libraries

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "GPL-3+"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/data.json "0.1.2"]
-                 [compojure "1.1.3"]
-                 [ring/ring-jetty-adapter "1.1.6"]]
-  :min-lein-version "2.1.0"
+  :dependencies [[org.clojure/clojure "1.8.0-RC4"]
+                 [org.clojure/data.json "0.2.6"]
+                 [compojure "1.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]]
+  :min-lein-version "2.5.0"
   :main substitute-lingrbot.core)
