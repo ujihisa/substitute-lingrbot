ec6ce8ef10b6e5f9ea78753deb72f0f798889590
ujihisa <ujihisa@gmail.com>
2016-01-21 07:00:47 -0800

    Use clojure 1.8.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "GPL-3+"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.8.0-RC4"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
                  [compojure "1.4.0"]
                  [ring/ring-jetty-adapter "1.4.0"]]
